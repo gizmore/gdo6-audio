74dc8db4906d13854cea3c29ceede518a5a2937d
gizmore <gizmore@wechall.net>
2017-08-24 20:14:55 +0200

    Initial commit.

---
+++ GDO_AudioFile.php
@@ -0,0 +1,31 @@
+<?php
+namespace GDO\Audio;
+
+use GDO\File\GDO_File;
+
+final class GDO_AudioFile extends GDO_File
+{
+	public function __construct()
+	{
+		parent::__construct();
+		$this->audioFile();
+	}
+	
+	public function audioFile()
+	{
+		$this->mime('audio/mp3');
+		$this->mime('audio/mpeg');
+		return $this;
+	}
+	
+	public function gdoAfterCreate()
+	{
+		$file = $this->getValue();
+		$info = Module_Audio::instance()->mp3Info($file);
+		$file->saveVars(array(
+			'file_duration' => $info[0],
+			'file_bitrate' => $info[1],
+		));
+	}
+	
+}
---
+++ Module_Audio.php
@@ -0,0 +1,32 @@
+<?php
+namespace GDO\Audio;
+
+use GDO\Core\Module;
+use GDO\File\File;
+use GDO\File\GDO_Path;
+use MP3File;
+
+final class Module_Audio extends Module
+{
+	public $module_priority = 30;
+	
+	public function getConfig()
+	{
+		return array(
+			GDO_Path::make('lame_mp3_path')->initial('/usr/bin/lame')->label('lamemp3_path'),
+		);
+	}
+	public function cfgLamePath() { return $this->getConfigValue('lame_mp3_path'); }
+
+	####################
+	### MP3File info ###
+	####################
+	public function mp3Info(File $file)
+	{
+		$this->includeClass('3p/MP3File');
+		$mp3file = new MP3File($file->getPath());
+		$duration = $mp3file->getDuration();
+		$bitrate = $mp3file->getBitrate();
+		return [$duration, $bitrate];
+	}
+}
